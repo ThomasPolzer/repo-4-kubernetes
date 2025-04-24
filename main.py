d1af47cb852a82e3f429260b232be20541f4afa9
thomaspolzer <tpolzer@gmx.net>
2025-04-24 12:12:11 +0200

    new version 03

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask Version 02 from Kubernetes CI/CD-Pipeline!'
+    return 'Web App with Python Flask Version 03 exclusive from Kubernetes CI/CD-Pipeline!'
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8080)
